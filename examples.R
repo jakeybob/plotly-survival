0e760e342a89bb587ef15d4eb8878f507c0990d8
Bob Taylor <j.boaby@gmail.com>
2018-09-13 17:15:46 +0100

    added example that shows up some bugs -- censor marks not grouped with survival line, if last point in line drops to zero then the upper half of the confidence interval is inverted

--- examples.R
+++ examples.R
@@ -9,8 +9,10 @@ source("plotly-survival.R")
 gg_p <- ggsurvplot(survfit(Surv(time, status) ~sex, conf.type="plain", conf.int=0.95, data = lung), conf.int = T)
 gg_p <- ggsurvplot(survfit(Surv(time, status) ~1, conf.type="plain", conf.int=0.95, data = lung), conf.int = T)
 
+gg_p <- ggsurvplot(survfit(Surv(time, status) ~ph.ecog, conf.type="plain", conf.int=0.95, data = lung), conf.int = T)
 
-plotly_survival(gg_p, strata_colours = tableau_color_pal("tableau10")(2),
+
+plotly_survival(gg_p, strata_colours = tableau_color_pal("Tableau 10")(4),
                           plot_title="Survival of Something",
                           xaxis=list(title="days"),
                           yaxis=list(title="surv", hoverformat=".2f"),
